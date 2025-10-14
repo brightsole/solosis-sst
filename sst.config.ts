72bffda6a63e5264a08e5a37c100cd5e09423b77
Drew Showalter <10665739+one19@users.noreply.github.com>
2025-10-14 18:20:29 +1100

    🔧 revise sst deployment checks.

--- sst.config.ts
+++ sst.config.ts
@@ -10,7 +10,7 @@ export default $config({
     };
   },
   async run() {
-    const itemsTable = new sst.aws.Dynamo('ItemsTable', {
+    const itemsTable = new sst.aws.Dynamo('Items', {
       fields: {
         id: 'string',
         ownerId: 'string',
